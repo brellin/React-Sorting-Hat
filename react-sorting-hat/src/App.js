8089b2272826991e4685658ce54c5ef2182bee33
brellin <cekakid@gmail.com>
2019-03-21 22:42:24 -0500

    Format completed and working

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -6,34 +6,70 @@ import Questions from './components/Questions/Questions';
 
 let questions = [
   {
-    output: 'You walk along a river and see something. Do you do something?',
-    id: 1,
-    answered: false
+    output: 'First',
+    id: '11',
+    answered: false,
+    answers: {
+      answerOne: 'Gryffindor',
+      answerTwo: 'Hufflepuff',
+      answerThree: 'Ravenclaw',
+      answerFour: 'Slytherin'
+    }
   },
   {
     output: 'Second',
-    id: 2,
-    answered: false
+    id: '12',
+    answered: false,
+    answers: {
+      answerOne: 'Gryffindor',
+      answerTwo: 'Hufflepuff',
+      answerThree: 'Ravenclaw',
+      answerFour: 'Slytherin'
+    }
   },
   {
     output: 'Third',
-    id: 3,
-    answered: false
+    id: '13',
+    answered: false,
+    answers: {
+      answerOne: 'Gryffindor',
+      answerTwo: 'Hufflepuff',
+      answerThree: 'Ravenclaw',
+      answerFour: 'Slytherin'
+    }
   },
   {
     output: 'Fourth',
-    id: 4,
-    answered: false
+    id: '14',
+    answered: false,
+    answers: {
+      answerOne: 'Gryffindor',
+      answerTwo: 'Hufflepuff',
+      answerThree: 'Ravenclaw',
+      answerFour: 'Slytherin'
+    }
   },
   {
     output: 'Fifth',
-    id: 5,
-    answered: false
+    id: '15',
+    answered: false,
+    answers: {
+      answerOne: 'Gryffindor',
+      answerTwo: 'Hufflepuff',
+      answerThree: 'Ravenclaw',
+      answerFour: 'Slytherin'
+    }
   },
   {
     output: 'Sixth',
-    id: 6,
-    answered: false
+    id: '16',
+    answered: false,
+    answers: {
+      answerOne: 'Gryffindor',
+      answerTwo: 'Hufflepuff',
+      answerThree: 'Ravenclaw',
+      answerFour: 'Slytherin'
+    }
   }
 ]
 
@@ -43,23 +79,40 @@ class App extends Component {
     this.state = {
       react: false,
       hat: false,
+      '0': false,
       '1': false,
       '2': false,
       '3': false,
       '4': false,
       '5': false,
-      '6': false
+      '6': false,
+      '11': 0,
+      '12': 0,
+      '13': 0,
+      '14': 0,
+      '15': 0,
+      '16': 0
     }
   }
 
-  initialize = e => {
+  initialize = () => {
     this.setState({
       hat: true,
-      '1': true
+      '0': true
+    });
+  }
+
+  submitAnswer = (qId, aId) => {
+    let qNum = qId.split('')[1].toString();
+    this.setState({
+      [qId]: aId,
+      [qNum]: true
     });
   }
 
   render() {
+    let houseNum = this.state['11'] + this.state['12'] + this.state['13'] + this.state['14'] + this.state['15'] + this.state['16'];
+
     return (
       <div className={`${this.state.react === true ? 'container' : ''}`}>
         <div
@@ -77,14 +130,21 @@ class App extends Component {
           initialize={this.initialize}
           hat={this.state.hat}
         />
+        <div className='toggle' style={{ display: this.state['16'] > 0 ? 'none' : 'flex' }}>
+          {questions.map((q, i) =>
+            <Questions hat={this.state.hat} question={q} key={i} bool={this.state[i]} submitAnswer={this.submitAnswer} />)}
+        </div>
 
         <div
-          className='questions'
-          style={{ display: this.state.hat === true ? 'flex' : 'none' }}
+          style={{ display: this.state['16'] > 0 ? 'flex' : 'none' }}
+          className='result'
         >
-          {questions.map((q, i) =>
-            <Questions hat={this.state.hat} question={q} key={i} bool={this.state[i + 1]} />)}
+          <h1>You have been sorted into {houseNum}!</h1>
         </div>
+
+        <footer style={{ display: this.state.react === true ? 'flex' : 'none' }}>
+          Copyright [William Charles Umstead Junior]
+        </footer>
       </div>
     );
   }
